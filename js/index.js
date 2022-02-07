301c93d81c7c9b224846655aa4fb953aff9f17f7
Shelby Grasso <shelbyagrasso@gmail.com>
2022-02-07 14:37:38 -0800

    iterating banner function added!!!

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
 "use strict";
 
-import 'bootstrap';
+window.onload = function() {
+
+    console.log("js file working:");
+
+var descriptorList = ['adaptor', 'strategist', 'connector', 'perfectionist', 'wordsmith', 'psychologist', 'comedian', 'virtual farmer', 'californian', 'life-long learner'];
+var currentDescriptorIndex = -1;
+
+function changeDescriptor() {
+    ++currentDescriptorIndex;
+    if (currentDescriptorIndex >= descriptorList.length) {
+        currentDescriptorIndex = 0;
+    }
+    document.getElementById("text-switch").innerHTML = descriptorList[currentDescriptorIndex];
+}
+var intervalID = setInterval(changeDescriptor, 2000);
+
+
+/*function advanceNewsItem() {
+    ++curNewsIndex;
+    if (curNewsIndex >= newsArray.length) {
+        curNewsIndex = 0;
+    }
+    setTickerNews(newsArray[curNewsIndex]);   // set new news item into the ticker
+}
+
+var intervalID = setInterval(advanceNewsItem, 5000);*/
+
+}
