de7e221ee0e2562136a82259d4a765128ff81aff
Shelby Grasso <shelbyagrasso@gmail.com>
2022-02-07 22:34:05 -0500

    about page frame built

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,12 @@
 
 window.onload = function() {
 
-    console.log("js file working:");
-
+/* Landing page */
+// end with adjective that is hard-coded into html on load
 var descriptorList = ['adaptor', 'strategist', 'connector', 'perfectionist', 'wordsmith', 'psychologist', 'comedian', 'virtual farmer', 'californian', 'life-long learner'];
 var currentDescriptorIndex = -1;
 
+// function to chnage banner adjectives every 2 seconds
 function changeDescriptor() {
     ++currentDescriptorIndex;
     if (currentDescriptorIndex >= descriptorList.length) {
@@ -14,17 +15,8 @@ function changeDescriptor() {
     }
     document.getElementById("text-switch").innerHTML = descriptorList[currentDescriptorIndex];
 }
+// intervalID keeps track of time interval until cleared
 var intervalID = setInterval(changeDescriptor, 2000);
 
-
-/*function advanceNewsItem() {
-    ++curNewsIndex;
-    if (curNewsIndex >= newsArray.length) {
-        curNewsIndex = 0;
-    }
-    setTickerNews(newsArray[curNewsIndex]);   // set new news item into the ticker
-}
-
-var intervalID = setInterval(advanceNewsItem, 5000);*/
 
 }
