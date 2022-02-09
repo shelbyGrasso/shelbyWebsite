b1325dbd3632cafe076e9643e0665eb645e936f1
Shelby Grasso <shelbyagrasso@gmail.com>
2022-02-09 18:55:50 -0500

    space bear!

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ function changeDescriptor() {
     if (currentDescriptorIndex >= descriptorList.length) {
         currentDescriptorIndex = 0;
     }
-    document.getElementById("text-switch").innerHTML = descriptorList[currentDescriptorIndex];
+    //document.getElementById("text-switch").innerHTML = descriptorList[currentDescriptorIndex];
 }
 // intervalID keeps track of time interval until cleared
 var intervalID = setInterval(changeDescriptor, 2000);
